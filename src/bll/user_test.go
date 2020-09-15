01fd7bf16fbfee9e07648dca834d3b01e6ed1475
mushroomsir <mushroomsir@163.com>
2020-09-15 17:26:53 +0800

    support label in label

--- src/bll/user_test.go
+++ src/bll/user_test.go
@@ -105,3 +105,73 @@ func TestUsers(t *testing.T) {
 		assert.Nil(err)
 	})
 }
+
+func TestChildLabelUserPercen(t *testing.T) {
+
+	user := &User{ms: model.NewModels(service.NewDB())}
+	product := &Product{ms: model.NewModels(service.NewDB())}
+
+	t.Run("childLabelUserPercen should work with apply rule", func(t *testing.T) {
+		assert := assert.New(t)
+		ctx := context.Background()
+
+		uid1 := tpl.RandUID()
+		user.BatchAdd(ctx, []string{uid1})
+		userObj, err := user.ms.User.Acquire(ctx, uid1)
+		assert.Nil(err)
+
+		productName := tpl.RandName()
+		productRes, err := product.Create(ctx, productName, productName)
+
+		label := &schema.Label{
+			ProductID: productRes.Result.ID,
+			Name:      tpl.RandName(),
+		}
+		err = user.ms.Label.Create(ctx, label)
+		assert.Nil(err)
+		labelRes, err := user.ms.Label.Acquire(ctx, productRes.Result.ID, label.Name)
+		assert.Nil(err)
+
+		labelRule := &schema.LabelRule{
+			ProductID: productRes.Result.ID,
+			LabelID:   labelRes.ID,
+			Kind:      schema.RuleUserPercent,
+			Rule:      `{"value": 100 }`,
+		}
+		assert.Equal(100, labelRule.ToPercent())
+		err = user.ms.LabelRule.Create(ctx, labelRule)
+		assert.Nil(err)
+
+		label2 := &schema.Label{
+			ProductID: productRes.Result.ID,
+			Name:      labelRes.Name + "-gray",
+		}
+		err = user.ms.Label.Create(ctx, label2)
+		assert.Nil(err)
+		labelRes2, err := user.ms.Label.Acquire(ctx, productRes.Result.ID, label2.Name)
+		assert.Nil(err)
+
+		labelRule2 := &schema.LabelRule{
+			ProductID: productRes.Result.ID,
+			LabelID:   labelRes2.ID,
+			Kind:      schema.RuleChildLabelUserPercent,
+			Rule:      `{"value": 100 }`,
+		}
+		assert.Equal(100, labelRule.ToPercent())
+		err = user.ms.LabelRule.Create(ctx, labelRule2)
+		assert.Nil(err)
+
+		userRes, err := user.ms.ApplyLabelRulesAndRefreshUserLabels(ctx, productRes.Result.ID, productName, userObj.ID, time.Now().UTC(), true)
+		assert.Nil(err)
+		userLabels := userRes.GetLabels(productName)
+		assert.True(len(userLabels) == 1)
+		assert.Equal(labelRes.Name, userLabels[0].Label)
+
+		userRes, err = user.ms.ApplyLabelRulesAndRefreshUserLabels(ctx, productRes.Result.ID, productName, userObj.ID, time.Now().UTC(), true)
+		assert.Nil(err)
+		userLabels = userRes.GetLabels(productName)
+		assert.True(len(userLabels) == 2)
+		assert.Equal(labelRes2.Name, userLabels[0].Label)
+		assert.Equal(labelRes.Name, userLabels[1].Label)
+	})
+}
